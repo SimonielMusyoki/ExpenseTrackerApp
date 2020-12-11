6413a391823bb892cb450f14ee61deee63e751c8
SimonielMusyoki <musyoki.mtk3@gmail.com>
2020-12-11 06:22:04 +0300

    Setup

---
+++ constants/icons.js
@@ -0,0 +1,31 @@
+export const baby_car_icon = require('../assets/icons/baby_car_icon.png');
+export const back_arrow_icon = require('../assets/icons/back_arrow_icon.png');
+export const calendar_icon = require('../assets/icons/calendar_icon.png');
+export const chart_icon = require('../assets/icons/chart_icon.png');
+export const cloth_icon = require('../assets/icons/cloth_icon.png');
+export const down_arrow = require('../assets/icons/down_arrow.png');
+export const education_icon = require('../assets/icons/education_icon.png');
+export const food_icon = require('../assets/icons/food_icon.png');
+export const healthcare_icon = require('../assets/icons/healthcare_icon.png');
+export const menu_icon = require('../assets/icons/menu_icon.png');
+export const more_icon = require('../assets/icons/more_icon.png');
+export const sports_icon = require('../assets/icons/sports_icon.png');
+export const up_arrow = require('../assets/icons/up_arrow.png');
+export const pin = require('../assets/icons/pin.png');
+
+export default {
+    baby_car_icon,
+    back_arrow_icon,
+    calendar_icon,
+    chart_icon,
+    cloth_icon,
+    down_arrow,
+    education_icon,
+    food_icon,
+    healthcare_icon,
+    menu_icon,
+    more_icon,
+    pin,
+    sports_icon,
+    up_arrow
+}
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+import icons from './icons'
+import { COLORS, SIZES, FONTS } from './theme';
+
+export { icons, COLORS, SIZES, FONTS};
